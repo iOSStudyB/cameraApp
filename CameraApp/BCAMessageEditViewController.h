e6b22d320a287e88a88821793324ddc1c3551213
neino3 <hideki.yokota@gmail.com>
2014-04-15 21:13:36 +0900

    add delegate method for UIImagePickerController
    - ひとまずカメラアプリが動く状態になるよう必要なメソッドを追加

--- CameraApp/BCAMessageEditViewController.h
+++ CameraApp/BCAMessageEditViewController.h
@@ -9,8 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface BCAMessageEditViewController : UIViewController<UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIActionSheetDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 - (IBAction)touchCancelItem:(UIBarButtonItem *)sender;
 - (IBAction)touchCameraItem:(UIBarButtonItem *)sender;
+- (IBAction)touchSaveItem:(UIBarButtonItem *)sender;
 
 @end
 @protocol BCAMessageEditViewDelegate
